3b2ddaf0e16d0806fde3e89c75c8457641a61e55
rmingon <ronanmingon69550@gmail.com>
2024-08-13 21:57:09 +0200

    up

--- software/src/main.cpp
+++ software/src/main.cpp
@@ -5,68 +5,54 @@
 
 Adafruit_SH1106 display(0); 
 
-const int pulsePin = A3; // Input signal connected to Pin 12 of Arduino
-
-int pulseHigh; // Integer variable to capture High time of the incoming pulse
-
-int pulseLow; // Integer variable to capture Low time of the incoming pulse
-
-float pulseTotal; // Float variable to capture Total time of the incoming pulse
-
-float frequency; // Calculated Frequency
-
-void setupTimer1() {
-  noInterrupts();
-  // Clear registers
-  TCCR1A = 0;
-  TCCR1B = 0;
-  TCNT1 = 0;
-
-  // 38004.750593824225 Hz (16000000/((420+1)*1))
-  OCR1A = 420;
-  // CTC
-  TCCR1B |= (1 << WGM12);
-  // Prescaler 1
-  TCCR1B |= (1 << CS10);
-  // Output Compare Match A Interrupt Enable
-  TIMSK1 |= (1 << OCIE1A);
-  interrupts();
-}
+const int ir1 = PINC3;
+const int ir2 = PINC2;
+const int ir3 = PINC1;
+const int ir4 = PINC0;
+const int ir5 = PIN_A7;
+const int ir6 = PIN_A6;
 
 void setup() {
   Serial.begin(115200);
 
-  pinMode(pulsePin, INPUT);
-  pinMode(2, OUTPUT);
+  pinMode(ir1, INPUT);
+  pinMode(ir2, INPUT);
+  pinMode(ir3, INPUT);
+  pinMode(ir4, INPUT);
+  pinMode(ir5, INPUT);
+  pinMode(ir6, INPUT);
+  
+  display.begin();
+  display.clearDisplay(); 
 
-  digitalWrite(2, LOW);
-  display.begin();  // initialisation de l'afficheur
-  display.clearDisplay();   // ça efface à la fois le buffer et l'écran
-
-  setupTimer1();
 }
 
 void loop() {
-  display.clearDisplay();   // ça efface à la fois le buffer et l'écran
+  display.clearDisplay();
 
-pulseHigh = pulseIn(pulsePin, HIGH);
-
-pulseLow = pulseIn(pulsePin, LOW);
-
-pulseTotal = pulseHigh + pulseLow; // Time period of the pulse in microseconds
-
-frequency = 1000000 / pulseTotal; // Frequency in Hertz (Hz)
-    
-      display.setCursor(30, 15);  // coordonnées du point de départ du texte
-      display.setTextColor(WHITE);
-      display.setTextSize(1);  // taille par défaut
-      display.println(analogRead(pulsePin));
-
-      display.display();  // affichage à l'écran
+  display.setTextColor(WHITE);
+  display.setTextSize(1); 
   
-}
+  display.setCursor(0, 50);
+  display.println(analogRead(ir1));
+  
+  display.setCursor(0, 30);
+  display.println(analogRead(ir2));
+  
+  display.setCursor(30, 10);
+  display.println(analogRead(ir3));
 
+  display.setCursor(80, 10);
+  display.println(analogRead(ir4));
+  
+  display.setCursor(100, 30);
+  display.println(analogRead(ir5));
+  
+  display.setCursor(100, 50);
+  display.println(analogRead(ir6));
 
-ISR(TIMER1_COMPA_vect) {
-  PORTD ^= (1 << PD2);
+  display.display();
+
+  delay(200);
+  
 }
